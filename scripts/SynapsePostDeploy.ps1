a96a1ef959c6477ec09650c3aae0b2f83823a388
Parveen Singh <parveen.singh@cloudacademy.com>
2022-10-06 13:28:21 -0700

    Updating URLs

--- scripts/SynapsePostDeploy.ps1
+++ scripts/SynapsePostDeploy.ps1
@@ -171,7 +171,7 @@ function Save-SynapseSampleArtifacts {
   $headers = @{ Authorization = "Bearer $token" }
 
   $synapseTokens = @{"`#`#azsynapsewks`#`#" = $SynapseWorkspaceName; }
-  $indexFileUrl = "https://raw.githubusercontent.com/CSALabsAutomation/azure-synapse-labs/main/environments/env1/Sample/index.json"
+  $indexFileUrl = "https://raw.githubusercontent.com/cloudacademy/ca-azure-ms-assets/main/Sample/index.json"
   $sampleCodeIndex = Invoke-WebRequest $indexFileUrl | ConvertFrom-Json
 
   foreach ($sampleArtifactCollection in $sampleCodeIndex) {
